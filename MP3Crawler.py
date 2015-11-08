d533152ae20e85ff849580431753cfc5d5488762
quentinxxz@gmail.com <quentinxxz@192.168.2.106>
2015-11-08 15:25:48 +0800

    save all the music result to disk in PlaylistParser.py

--- MP3Crawler.py
+++ MP3Crawler.py
@@ -18,7 +18,7 @@ class MP3Crawler:
     def __init__(self,crawlername,workerThreadNum,
         pollInterval=0.5,pollTimeout=None,downloadTimeout=5):
         self.threadPool = ThreadPool(workerThreadNum)
-        self.crawlername=crawlername
+        self.crawlername = crawlername
         self.pollTimeout = pollTimeout 
         self.crawlerThread = CrawlerThread(self.threadPool,pollTimeout)
         self.mp3Downloader = MP3Downloader(downloadTimeout)
@@ -33,6 +33,10 @@ class MP3Crawler:
         self.crawlerThread.dismiss()
         self.crawlerThread.join()
 
+    def waitUtilComplete(self):
+        '''wait until all tasks complete'''
+        self.threadPool.wait()
+
     def __checkTask(self,task):
         if task.has_key('type')==False or task.has_key('url') ==False:
            return False
@@ -46,8 +50,8 @@ class MP3Crawler:
     def downloadMP3(self,url,filePath):
         self.mp3Downloader.downloadM(url, filePath)
 
+
     def __printResult(self,request,result):
-        print "calllllllllllllllllllllllllllll"
         print "---Result from request %s : %r" % (request.requestID,result)
         #pass
 
--- PlaylistParser.py
+++ PlaylistParser.py
@@ -2,9 +2,24 @@
 #coding=utf-8
 import json
 import os
+from MP3Crawler import *
+
+playListInfoDir="playListInfo/"
+musicInfoDir="musicInfo/"
+mp3Dir="mp3/"
 
 class PlaylistParser(object):
     """This class is used to parser json result """
+
+    @classmethod
+    def init(cls):
+    	if not os.path.isdir(playListInfoDir):
+    		os.mkdir(playListInfoDir)
+    	if not os.path.isdir(musicInfoDir):
+    		os.mkdir(musicInfoDir)
+    	if not os.path.isdir(mp3Dir):
+    		os.mkdir(mp3Dir)
+
 
     @classmethod
     def parser(cls,jsonText):
@@ -20,11 +35,8 @@ class PlaylistParser(object):
     	playListInfo= josnData['result'].copy()
     	del playListInfo['tracks']
     	id = playListInfo['id']  ##TODO  raise some  exception 
-    	dirPath = "playListInfo/"
     	if id :
-    		if not os.path.isdir(dirPath):
-    			os.mkdir(dirPath)
-    		playListInfoFie = open(dirPath+str(id)+'.txt', 'w') 
+    		playListInfoFie = open(playListInfoDir+str(id)+'.txt', 'w') 
     		playListInfoFie.write(str(playListInfo))
     		playListInfoFie.close( )
     		return True
@@ -33,22 +45,53 @@ class PlaylistParser(object):
     		return False
 
 
-
-
     @classmethod 
-    def downloadMusics(cls,jsonData):
+    def downloadMusics(cls,jsonData,mp3Crawler):
     	''''download mp3 music and save music info '''
     	tracks = jsonData['result']['tracks']
     	for track in tracks: 
-    		print track
-   
+    		PlaylistParser.__saveMusic(track,mp3Crawler)
+    		
+
+
+    @classmethod 
+    def __saveMusic(cls,track,mp3Crawler):
+    	'''save a mp3 music info as file 
+    		##TODO save into database
+    	   and save mp3 file
+    	'''
+    	id = track['id']  ##TODO  raise some  exception 
+    	if  id :
+    		musicInfoFie = open(musicInfoDir+str(id)+'.txt', 'w') 
+    		musicInfoFie.write(str(track))
+    		musicInfoFie.close( )
+    	else:
+    		print 'save music info fail'
+    		return False 
+
+    	task={'savePath':mp3Dir + str(id)+'.mp3','url':track['mp3Url'],'type':'mp3'}
+    	mp3Crawler.addTask(task)
+    	return True
+
+
 
 if __name__ == '__main__':
+	import time
+
 	fileObject = open('result.json')
 	try:
 		jsonText = fileObject.read( )
 	finally:
 		fileObject.close( )
+
+	crawler = MP3Crawler("MP3Crawler",10)
+	crawler.start()
+    	
+	PlaylistParser.init()
 	jsonData = PlaylistParser.parser(jsonText)
-	PlaylistParser.savePlayListInfo(jsonData)
-	#PlaylistParser.downloadMusics(jsonData)
+	if PlaylistParser.savePlayListInfo(jsonData):
+		PlaylistParser.downloadMusics(jsonData,crawler)
+		
+	crawler.threadPool.wait()
+
+
